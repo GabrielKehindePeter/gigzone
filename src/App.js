db6327b5c27212c70c718fed8de483d8bfc3fdf5
Gabriel Kehinde Peter <96505592+GabrielKehindePeter@users.noreply.github.com>
2025-02-15 15:39:08 +0100

    deployment

--- src/App.js
+++ src/App.js
@@ -3,28 +3,25 @@ import './App.css';
 import Navbar from './Composer/Navbar';
 import Home from './Composer/home';
 import Footer from './Composer/footer';
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';  // No BrowserRouter here!
 import About from './Composer/About';
 import HowItWorks from './Composer/HowItWork';
 import Jobs from './Composer/Jobs';
 import Contact from './Composer/Contact';
+
 function App() {
   return (
-    <Router>
     <div className="App">
-    <Navbar></Navbar>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/howitworks" element={<HowItWorks/>} />
-      <Route path="/jobs" element={<Jobs/>} />
-      <Route path="/contact" element={<Contact/>} />
-  
-    </Routes>
-    
-    <Footer></Footer>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/howitworks" element={<HowItWorks />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </div>
-    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+const basename = process.env.NODE_ENV === 'production' ? '/gigzone' : '';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/gigzone">  {/* Add basename here */}
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
